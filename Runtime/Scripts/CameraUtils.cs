3ec5a4ec18e65b0d0d79196b751547d29bb3f8e6
Dmitry Mozgin <m0391n@gmail.com>
2020-01-01 00:15:35 +0300

    Added CameraUtils.

---
+++ Runtime/Scripts/CameraUtils.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace m039.Common
+{
+
+	public static class CameraUtils
+	{
+
+		public static Bounds GetMainCameraBounds()
+		{
+			var size = new Vector3();
+
+			size.y = Camera.main.orthographicSize * 2;
+			size.x = size.y * Camera.main.aspect;
+
+			return new Bounds
+			(
+				center: (Vector2)Camera.main.transform.position,
+				size: size
+			);
+		}
+
+	}
+
+}
--- Runtime/Scripts/CommonUtils.cs
+++ Runtime/Scripts/CommonUtils.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 namespace m039.Common
 {
 
-    public static class CommonUtils 
-    {
-        static public void DestroyAllChildren(Transform transform)
-        {
-            if (transform == null)
-            {
-                return;
-            }
+	public static class CommonUtils
+	{
+		static public void DestroyAllChildren(Transform transform)
+		{
+			if (transform == null)
+			{
+				return;
+			}
 
-            var toDestroy = new List<GameObject>();
+			var toDestroy = new List<GameObject>();
 
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                toDestroy.Add(transform.GetChild(i).gameObject);
-            }
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				toDestroy.Add(transform.GetChild(i).gameObject);
+			}
 
-            toDestroy.ForEach((obj) => GameObject.DestroyImmediate(obj));
-        }
-    }
+			toDestroy.ForEach((obj) => GameObject.DestroyImmediate(obj));
+		}
+	}
 
 }
