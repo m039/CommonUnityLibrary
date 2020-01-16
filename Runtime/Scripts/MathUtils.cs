fe2435857406286c72c970faf7890e51b4a82fb6
Dmitry Mozgin <m0391n@gmail.com>
2020-01-16 14:12:21 +0300

    Added MathUtils.

---
+++ Runtime/Scripts/MathUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace m039.Common 
+{
+
+	public class MathUtils: MonoBehaviour 
+	{ 
+
+		public static float Max(float a1, float a2, float a3)
+        {
+            return Mathf.Max(Mathf.Max(a1, a2), a3);
+        }
+
+	}
+
+}
