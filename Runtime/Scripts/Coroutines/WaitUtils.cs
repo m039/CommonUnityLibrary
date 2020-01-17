0d622b23a4414cb3392a9a9943918b9acc2b20c1
Dmitry Mozgin <m0391n@gmail.com>
2020-01-17 20:57:58 +0300

    Added WaitUtils.

---
+++ Runtime/Scripts/Coroutines/WaitUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace m039.Common
+{
+
+	public static class WaitUtils
+	{
+
+        /// A version of WaitForSeconds but can be interrupted.
+        public static CustomYieldInstruction WaitForSeconds(float seconds, Func<bool> isInterrupted)
+        {
+            if (isInterrupted == null || isInterrupted() || seconds < 0 )
+                return null;
+
+            var endTime = Time.time + seconds;
+            return new WaitWhile(() => endTime > Time.time && !isInterrupted());
+        }
+
+	}
+
+}
