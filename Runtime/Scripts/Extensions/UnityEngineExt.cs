b6cd5220b35522045ca2a45a64c896ff78c2ca33
Dmitry Mozgin <m0391n@gmail.com>
2020-09-15 20:01:30 +0300

    Added extension methods to Vector3

--- Runtime/Scripts/Extensions/UnityEngineExt.cs
+++ Runtime/Scripts/Extensions/UnityEngineExt.cs
@@ -32,6 +32,24 @@ namespace m039.Common
             color.a = alpha;
             return color;
         }
+
+        public static Vector3 WithX(this Vector3 vector, float x)
+        {
+            vector.x = x;
+            return vector;
+        }
+
+        public static Vector3 WithY(this Vector3 vector, float y)
+        {
+            vector.y = y;
+            return vector;
+        }
+
+        public static Vector3 WithZ(this Vector3 vector, float z)
+        {
+            vector.z = z;
+            return vector;
+        }
     }
 
 }
