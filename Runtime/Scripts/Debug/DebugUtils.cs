a1de678129bfc092afe97b33ab2546d25618956e
Dmitry Mozgin <m0391n@gmail.com>
2021-03-21 12:28:52 +0300

    Added DebugUtils

---
+++ Runtime/Scripts/Debug/DebugUtils.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+namespace m039.Common
+{
+
+    public static class DebugUtils
+    {
+        public static void ClearLog()
+        {
+            var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+            var type = assembly.GetType("UnityEditor.LogEntries");
+            var method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
+        }
+    }
+
+}
