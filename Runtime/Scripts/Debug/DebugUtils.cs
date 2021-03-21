6f3019083b4f6b8ec2b327cdb691c1ca4542a5e8
Dmitry Mozgin <m0391n@gmail.com>
2021-03-22 00:35:32 +0300

    Hotfix

--- Runtime/Scripts/Debug/DebugUtils.cs
+++ Runtime/Scripts/Debug/DebugUtils.cs
@@ -7,10 +7,12 @@ namespace m039.Common
     {
         public static void ClearLog()
         {
+#if UNITY_EDITOR
             var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
             var type = assembly.GetType("UnityEditor.LogEntries");
             var method = type.GetMethod("Clear");
             method.Invoke(new object(), null);
+#endif
         }
     }
 
