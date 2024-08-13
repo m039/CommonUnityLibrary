5a72183ec05aa758f4548b969ec9fe911917be44
Dmitry Mozgin <m0391n@gmail.com>
2024-08-13 05:10:08 +0300

    Update

---
+++ Runtime/Scripts/AI/GOAP/Agent.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace m039.Common.GOAP
+{
+    public class Agent
+    {
+        public HashSet<AgentAction> actions = new();
+
+        public HashSet<AgentGoal> goals = new();
+
+        public Dictionary<string, AgentBelief> beliefs = new();
+
+        ActionPlan _actionPlan;
+
+        AgentAction _currentAction;
+
+        AgentGoal _currentGoal;
+
+        AgentGoal _lastGoal;
+
+        readonly Planner _planner = new();
+
+        public void Update()
+        {
+            if (_currentAction != null)
+            {
+                if (_currentAction.complete)
+                {
+                    _currentAction.Stop();
+                    _currentAction = null;
+
+                    if (_actionPlan.actions.Count <= 0)
+                    {
+                        _actionPlan = null;
+                    }
+                }
+                else
+                {
+                    _currentAction.Update(Time.deltaTime);
+                }
+            } else
+            {
+                CalculatePlan();
+            }
+
+            if (_actionPlan != null && _actionPlan.actions.Count > 0 && _currentAction == null)
+            {
+                _currentAction = _actionPlan.actions.Pop();
+
+                if (_currentAction.preconditions.All(p => p.Evaluate()))
+                {
+                    _currentAction.Start();
+                } else
+                {
+                    _currentAction = null;
+                    _currentGoal = null;
+                    _actionPlan = null;
+                }
+            }
+        }
+
+        public void ClearState()
+        {
+            _currentAction = null;
+            _currentGoal = null;
+            _actionPlan = null;
+        }
+
+        void CalculatePlan()
+        {
+            var priorityLevel = _currentGoal?.priority ?? 0;
+
+            HashSet<AgentGoal> goalsToCheck = goals;
+
+            if (_currentGoal != null)
+            {
+                goalsToCheck = new (goals.Where(g => g.priority > priorityLevel));
+            }
+
+            var potentialPlan = _planner.Plan(this, goalsToCheck, _lastGoal);
+            if (potentialPlan != null)
+            {
+                _actionPlan = potentialPlan;
+            }
+        }
+    }
+
+}
---
+++ Runtime/Scripts/AI/GOAP/Planner.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace m039.Common.GOAP
+{
+    public class Planner
+    {
+        public ActionPlan Plan(Agent agent, HashSet<AgentGoal> goals, AgentGoal mostRecentGoal = null)
+        {
+            List<AgentGoal> orderedGoals = goals
+                .Where(g => g.desiredEffects.Any(b => !b.Evaluate()))
+                .OrderByDescending(g => g == mostRecentGoal ? g.priority - 0.01 : g.priority)
+                .ToList();
+
+            foreach (var goal in orderedGoals)
+            {
+                Node goalNode = new Node(null, null, goal.desiredEffects, 0);
+
+                if (FindPath(goalNode, agent.actions))
+                {
+                    if (goalNode.isLeafDead) continue;
+
+                    Stack<AgentAction> actionStack = new();
+                    while (goalNode.leaves.Count > 0)
+                    {
+                        var cheapestLeaf = goalNode.leaves.OrderBy(leaf => leaf.cost).First();
+                        goalNode = cheapestLeaf;
+                        actionStack.Push(cheapestLeaf.action);
+                    }
+
+                    return new ActionPlan(goal, actionStack, goalNode.cost);
+                }
+            }
+
+            return null;
+        }
+
+        bool FindPath(Node parent, HashSet<AgentAction> actions)
+        {
+            var orderedActions = actions.OrderBy(a => a.cost);
+            var requiredEffects = parent.requiredEffects;
+
+            requiredEffects.RemoveWhere(b => b.Evaluate());
+
+            if (requiredEffects.Count == 0)
+            {
+                return true;
+            }
+
+            foreach (var action in orderedActions)
+            {
+                if (action.effects.Any(requiredEffects.Contains))
+                {
+                    var newRequiredEffects = new HashSet<AgentBelief>(requiredEffects);
+                    newRequiredEffects.ExceptWith(action.effects);
+                    newRequiredEffects.UnionWith(action.preconditions);
+
+                    var newAvailableActions = new HashSet<AgentAction>(actions);
+                    newAvailableActions.Remove(action);
+
+                    var newNode = new Node(parent, action, newRequiredEffects, parent.cost + action.cost);
+
+                    if (FindPath(newNode, newAvailableActions))
+                    {
+                        parent.leaves.Add(newNode);
+                        newRequiredEffects.ExceptWith(newNode.action.preconditions);
+                    }
+
+                    if (newRequiredEffects.Count == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        class Node
+        {
+            public Node parent { get; }
+
+            public AgentAction action { get; }
+
+            public HashSet<AgentBelief> requiredEffects { get; }
+
+            public List<Node> leaves { get; }
+
+            public float cost { get; }
+
+            public bool isLeafDead => leaves.Count == 0 && action == null;
+
+            public Node(Node parent, AgentAction action, HashSet<AgentBelief> effects, float cost)
+            {
+                this.parent = parent;
+                this.action = action;
+                requiredEffects = new HashSet<AgentBelief>(effects);
+                leaves = new List<Node>();
+                this.cost = cost;
+            }
+        }
+    }
+}
